4019398ff3ff4dafb0dd11423a0273f7e53f9272
MatheusFernandoMagalhaes <matheusf.magalhaes@hotmail.com>
2022-07-19 17:12:15 -0300

    Requisito 4 feito

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import NotFound from '../pages/NotFound';
+
+describe('Teste o componente <NotFound.js />', () => {
+  test('Se a página contém um heading h2 com o texto Page requested not found 😭;', () => {
+    render(<NotFound />);
+    const notFound = screen.getByText(/Page requested not found/i);
+    expect(notFound).toBeInTheDocument();
+  });
+  test('Teste se a página mostra a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif', () => {
+    render(<NotFound />);
+    const sadPikachu = screen.getByAltText(/pikachu crying/i);
+    expect(sadPikachu.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
